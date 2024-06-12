dd3c77952f5a068ecbe79740ff119219d14c7b26
kkademorais <kaike.carvalho@outlook.com>
2024-06-12 03:31:21 -0300

    Desafio project in progress

--- src/DesafioControleFluxo/Contador.java
+++ src/DesafioControleFluxo/Contador.java
@@ -1,4 +1,29 @@
 package DesafioControleFluxo;
+import java.util.Scanner;
+import java.util.Locale;
 
 public class Contador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.printf("\nDigite o primeiro parâmetro: ");
+        int parametroUm = scanner.nextInt();
+        System.out.printf("\nDigite o segundo parâmetro: ");
+        int parametroDois = scanner.nextInt();
+
+        try{
+            //chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+        }catch(){
+            System.out.printf("\nO segundo parâmetro deve ser MAIOR que o primeiro!!");
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+
+        int contagem = parametroDois - parametroUm;
+        //realizar o for para imprimir os números com base na variável contagem
+    }
 }
